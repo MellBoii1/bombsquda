ba3188b0310c30ce8b0adba8da02161eeaa8c2c7
GummyBoi_YT <150540342+GummyBoiYT1@users.noreply.github.com>
2026-07-06 12:17:00 -0400

    WATER COOLER 3/5

---
+++ ba_data/python/fromgoverhaul/watercooler.py
@@ -0,0 +1,127 @@
+import bascenev1 as bs
+from bascenev1lib.gameutils import SharedObjects
+
+class WaterCoolerSpawner(bs.Actor):
+    """ just stands there and despawns if not touched
+    
+    if it does get touched it spawns the regaulr guy
+    """
+
+    def __init__(self, position: tuple[float, float, float]):
+        super().__init__()
+        self.can_spawn = False
+        self.node = bs.newnode(
+            'prop',
+            delegate=self,
+            attrs={
+                'position': (position[0], position[1] + 1, position[2]),
+                'mesh': bs.getmesh('tnt'),
+                'body': 'puck',
+                'color_texture': bs.gettexture('tnt'),
+                'reflection': 'soft',
+                'shadow_size': 0.5,
+                'position': (0, 1, 0),
+                'mesh_scale': 1.0,
+                'gravity_scale': 1.5,
+                'materials': [
+                    SharedObjects.get().only_collide_with_floor_mat
+                ],
+            },
+        )
+        # ????
+        self.node.position = (position[0], position[1] + 1, position[2])
+        # The one and only chance to fight the water cooler this round
+        self.getactivity().water_cooler_spawned = True
+        bs.timer(2, self.allow_attacks)
+        bs.timer(15, bs.Call(self.handlemessage, bs.DieMessage()))
+       
+    
+    def allow_attacks(self):
+        mat = bs.Material()
+        from bascenev1lib.actor.spazfactory import SpazFactory
+        mat.add_actions(
+                actions=(
+                    ('modify_part_collision', 'collide', False),
+                ),
+            )
+        mat.add_actions(
+                conditions=('they_have_material', SpazFactory.get().spaz_material),
+                actions=(
+                    ('modify_part_collision', 'collide', True),
+                ),
+            )
+        mat.add_actions(
+                conditions=('they_have_material', SharedObjects.get().footing_material),
+                actions=(
+                    ('modify_part_collision', 'collide', True),
+                ),
+            )
+        self.node.materials = []
+        self.can_spawn = True
+        
+
+    def exists(self):
+        return bool(self.node)
+    
+    def handlemessage(self, msg):
+        if isinstance(msg, bs.DieMessage):
+            self.can_spawn = False
+            self.tick_timer = None
+            if not self.exists():
+                return
+            if msg.immediate:
+                self.node.delete()
+            else:
+                bs.animate(
+                    self.node, 
+                    attr='mesh_scale', 
+                    keys={0: self.node.mesh_scale, 0.5: 0.0})
+                bs.timer(0.5, self.node.delete)
+        elif isinstance(msg, bs.HitMessage):
+            if not self.exists():
+                return
+            # spawn the regular watercooler
+            if self.can_spawn and self.node:
+                
+                
+                WaterCooler(position=self.node.position).autoretain()
+                self.handlemessage(bs.DieMessage(True))
+        elif isinstance(msg, bs.OutOfBoundsMessage):
+            self.handlemessage(bs.DieMessage(True))
+        return super().handlemessage(msg)
+
+class WaterCooler(bs.Actor):
+    """ deltarune water cooler"""
+    
+    def __init__(self, position: tuple[float, float, float]):
+        super().__init__()
+       
+        self.hp = 1700 * (10)
+        self.node = bs.Node(None)
+   
+    
+
+        bs.setmusic(getattr(bs.MusicType, 'WATERCOOLER'))
+        self.node = bs.newnode(
+            'prop',
+            delegate=self,
+            attrs={
+                'position': (position[0], position[1] + 1, position[2]),
+                'mesh': bs.getmesh('tnt'),
+                'body': 'puck',
+                'color_texture': bs.gettexture('tnt'),
+                'reflection': 'soft',
+                'shadow_size': 0.5,
+                'position': (0, 1, 0),
+                'mesh_scale': 1.0,
+                'gravity_scale': 1.5,
+                'materials': [
+                    SharedObjects.get().only_collide_with_floor_mat
+                ],
+            },
+        )
+    
+    def exists(self):
+        return bool(self.node)
+    def is_alive(self):
+        return self.hp > 0
